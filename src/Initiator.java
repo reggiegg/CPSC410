c0833536f62d3b30dae2efcb5b2c101104416741
reggiegg <reggie.gillett@gmail.com>
2014-10-16 19:54:20 -0700

    Added JavaNCSS call. It works returning javancss-statistics.xml to the working directory. Added a few things to the .gitignore

--- src/Initiator.java
+++ src/Initiator.java
@@ -18,30 +18,46 @@ public class Initiator {
 		
 		if (args.length != 2){
 			printUsageInstructions();
-		}else{							
+		}else{
+			
+			String paths = "/home/reggie/Documents/CPSC410/spamassassin/trunk/logfile.log /home/reggie/Documents/CPSC410/spamassassin/trunk/";
+
 			File logfile =  new File(args[0]);
 			File modulepath = new File(args[1]);
+					
 			
-			Runtime rt = Runtime.getRuntime();
-			
-			Process statSVNProcess;
 			try {
-				statSVNProcess = rt.exec("java -jar statsvn.jar -xml "+logfile.getPath()+" "+modulepath.getPath());
+				
+				Runtime rt = Runtime.getRuntime();
+				Process statSVNProcess = rt.exec("java -jar statsvn.jar -xml "+logfile.getPath()+" "+modulepath.getPath());
 				statSVNProcess.waitFor();
+				System.out.println("??");
 				
 				if (statSVNProcess.exitValue() != 0){
 					throw new IOException("statSVN did not properly execute.");
 				}
-			
-				File repostats = new File("./repo-statistics.xml");
 				
+				statSVNProcess.destroy();
+						
+				File statSVNStats = new File("./repo-statistics.xml");
+							
 				// StatSVNParser statSVNParser = new StatSVNParser();  // something like this for the parsing stage
-				// statSVNParser.parse(repostats);
+				// statSVNParser.parse(statSVNStats);
+								
+				File javaNCSSStats = new File("javancss-statistics.xml");
+				
+				Process javaNCSSProcess = rt.exec("javancss-32.53/bin/javancss -xml -all -out "+javaNCSSStats.getAbsolutePath()+" "+modulepath.getAbsolutePath());
+				javaNCSSProcess.waitFor();
+				
+				if (javaNCSSProcess.exitValue() != 0){
+					throw new IOException("javaNCSS did not properly execute.");
+				}
+				javaNCSSProcess.destroy();
+				
 				
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 									
